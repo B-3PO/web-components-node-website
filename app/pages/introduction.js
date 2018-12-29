e5132ba6887d4005e0429729b6600d197c34b992
Ben Rubin <ben@benjaminrubin.com>
2018-12-29 13:17:39 -0600

    update introduction

--- app/pages/introduction.js
+++ app/pages/introduction.js
@@ -19,16 +19,16 @@ const page = customElements.export('introduction-page', class extends HTMLElemen
         <article>
           <article class="into-article">
             <h3>Introduction</h3>
-            <p>
-              This project is meant to serve as a way to use web components in place of a framework. By no means is this meant to act as a replacement for frameworks (React, Angular, Vue, ...). If you are looking for something simple, performant, with server-side rendering support then this project may interest you.
-            </p>
-          </article>
+            <p>This projects uses web components. If you are not familiar with the web-component specs then I would suggest reading up on them before moving forward. <a href="https://developer.mozilla.org/en-US/docs/Web/Web_Components">Link</a></p>
 
-          <article class="sub-article">
-            <h4>Goals</h4>
+            <h5>What</h5>
+            <p>web-components-node is a set of tools to allow web pages to be built using web components. The tools add some great features including server-side rendering, dynamic client side rendering, performance (using native browser features), optimizations that reduces load times, managed client side caching, pwa compatable, and more.</p>
 
+            <h5>Why</h5>
+            <p>Browsers, javascript, css, and html are evolving in a uniform and predictable pace. Many of features that are provided by frameworks(react, angular, vue, jquery) are now available in native forms in the browser. It is time to start looking at building web sites and applications the way we used to, nativly and with out loads of dependencies. With web-components spec becoming V1 we now have one of the last major features that frameworks provide in a native form (custom html with code associated to it). In short lets learn / return to javascript, css, and html ats it core</p>
+
+            <h5>Goals</h5>
             <img src="assets/images/goals-performance.png" alt="" width=100% style="padding-top:24px; padding-bottom: 32px;" />
-
             <div class="row">
               <div class="flex column-article">
                 <h6>Perform</h6>
@@ -45,22 +45,12 @@ const page = customElements.export('introduction-page', class extends HTMLElemen
               </div>
 
               <div class="flex column-article">
-                <h6>Reduce</h6>
+                <h6>Reduce Reduce Reduce</h6>
                 <p>
                   Reduce the number of dependencies needed to create a web application
                 </p>
               </div>
             </div>
-          </article>
-
-          <article class="sub-article">
-            <h4>Why?</h4>
-            <p>
-              At the time I created this repo I was in the process of interviewing for a new job. Almost every company asked me what frameworks I preferred. My development experience in the web started prior to frameworks being available, so when I thought about the question of which framework, the only answer I could confidently say was "I don't prefer any of them". This does not mean I do not see value in frameworks or do not enjoy working in them, because I do see value and have built some great projects with some of them. But now you can find native alternatives to many of the features that frameworks fulfilled.
-            </p>
-            <p>
-              I decided to take a hard look at how I might build my next web application. What you see here is the fruits of my research. I realized that web components solve one of the biggest problems that could not be achieved without a framework in the past. The ability to scope code to a block of HTML. Almost all the other features needed to work in the web (querySelector, template literals, ...) are available natively in the majority of browsers today.
-            </p>
           </article>
 
         </article>
